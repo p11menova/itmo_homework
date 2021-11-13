56f20efdaeedb09dc3b7a2b6908f0d450978afdd
Ekaterina Pimenova <74774234+p11menova@users.noreply.github.com>
2021-11-13 17:41:35 +0300

    Add files via upload

--- Texts.py
+++ Texts.py
@@ -11,6 +11,8 @@ class Line:
         return len(self.__data.split())
 
     def word(self, number):
+        if number > len(self.__data):
+            raise IndexError('операция получения слова невозможна')
         return self.__data.split()[number]
 
 
@@ -22,13 +24,18 @@ class Text:
         return len(self.__text)
 
     def line_by_num(self, num):
+        if num > len(self.__text):
+            raise IndexError('операция получения строки невозможна')
         return self.__text[num].data
 
     def words_in_line(self, num):
         return self.__text[num].words
 
-    def one_word_by_num(self, strnum, wordnum):
-        return self.__text[strnum].word(wordnum)
+    def one_word_by_num(self, strnum: int, wordnum: int):
+        try:
+            return self.__text[strnum].word(wordnum)
+        except IndexError as e:
+            print('операция получения слова про номеру невозможна:', e)
 
 
 class EditableText(Text):
@@ -37,13 +44,22 @@ class EditableText(Text):
         self.TEXT = list(map(lambda x: Line(data=x), data))
 
     def change_str(self, num, another_str):
+        if num > len(self.TEXT):
+            raise IndexError('операция замены строки невозможна')
         self.TEXT[num] = Line(another_str)
 
     def change_word(self, strnum, wordnum, another_word):
-        self.TEXT[strnum].data[wordnum] = another_word
+        try:
+            self.TEXT[strnum].data[wordnum] = another_word
+        except IndexError as e:
+            print('операция замены слова невозможна: ', e)
 
     def word_position(self, word):
-        return list(map(lambda x: x.data, self.TEXT)).index(word)
+        try:
+            return list(map(lambda x: x.data, self.TEXT)).index(word)
+        except IndexError as e:
+            print('операция получения слова в тексте невозможна: ', e)
+
     @property
     def text(self):
         return '/n'.join(list(map(lambda x: x.data, self.TEXT)))
