847391a33c26d0f0410d306fbfe61813af29676a
Ekaterina Pimenova <74774234+p11menova@users.noreply.github.com>
2021-11-27 23:26:25 +0300

    main task

--- Assessment/pokemon.py
+++ Assessment/pokemon.py
@@ -2,13 +2,11 @@ import requests
 from typing import List
 from itmo_test.pokemon_models import *
 
-POKEMONS = []
 
-
-def get_content() -> List:
+def get_content(limit=700) -> List:
     pokemon_content = []
     try:
-        response = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=50').json()
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/', params={'limit': limit}).json()
         pokemon_content = response['results']
 
     except requests.exceptions.RequestException as err:
@@ -17,14 +15,29 @@ def get_content() -> List:
     return pokemon_content
 
 
-def main():
-    for i in get_content():
+def make_pokemons(content: List) -> None:
+    for i in content:
+        BASE_POKEMONS.append(BasePokemon(name=i['name']))
+
         data = requests.get(i['url']).json()
         POKEMONS.append(Pokemon(id=data['id'],
                                 name=i['name'],
                                 height=data['height'],
                                 weight=data['weight']))
 
+def main():
+    #task 1
+    make_pokemons(get_content())
+    pok_api = PokeAPI()
+    print(pok_api.get_pokemon(key='ditto'))
+
+    #task 2
+    make_pokemons(get_content(50))
+    pok_api2 = PokeAPI()
+    poks = sorted(pok_api2.get_all(get_full=True), key=lambda x: x.get_weight(), reverse=True)
+    print(poks[0])
+
 
 main()
+
 
---
+++ Assessment/pokemon_models.py
@@ -0,0 +1,58 @@
+BASE_POKEMONS = []
+POKEMONS = []
+
+
+class BasePokemon:
+    def __init__(self, name: str) -> None:
+        self.__name = name
+
+    def __str__(self) -> str:
+        return f'name: {self.__name}'
+
+    def get_name(self) -> str:
+        return self.__name
+
+
+class Pokemon(BasePokemon):
+    def __init__(self, id:int, name:str, height:int, weight:int) -> None:
+        BasePokemon.__init__(self, name)
+
+        self.__id = id
+        self.__name = name
+        self.__height = height
+        self.__weight = weight
+
+    def __str__(self) -> str:
+        return f'id: {self.__id}, name: {self.__name}, ' \
+            f'height: {self.__height}, weight: {self.__weight}'
+
+    def get_id(self) -> int:
+        return self.__id
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_weight(self) -> int:
+        return self.__weight
+
+
+class PokeAPI:
+    def get_pokemon(self, key: int or str) -> str:
+        try:
+            if isinstance(key, int):
+                return list(filter(lambda x: x.get_id() == key, POKEMONS))[0]
+            elif isinstance(key, str):
+                return list(filter(lambda x: x.get_name() == key, POKEMONS))[0]
+        except IndexError:
+            return 'такого покемона нет в списке!'
+
+    def get_all(self, get_full: bool):
+        if not get_full:
+            return '\n'.join([str(j) for j in BASE_POKEMONS])
+
+        cur_num = 1
+        pokems_num = len(POKEMONS)
+
+        while cur_num <= pokems_num:
+            yield self.get_pokemon(key=cur_num)
+            cur_num += 1
