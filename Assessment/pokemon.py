621b17231c93b6ccd39fe342f10f88454d2b419a
Ekaterina Pimenova <74774234+p11menova@users.noreply.github.com>
2021-12-01 17:45:08 +0300

    Pokemon.stats

--- Assessment/pokemon.py
+++ Assessment/pokemon.py
@@ -17,26 +17,33 @@ def get_content(limit=700) -> List:
 
 def make_pokemons(content: List) -> None:
     for i in content:
-        BASE_POKEMONS.append(BasePokemon(name=i['name']))
-
+        BASE_POKEMONS.append(BasePokemon(i['name']))
         data = requests.get(i['url']).json()
-        POKEMONS.append(Pokemon(id=data['id'],
-                                name=i['name'],
-                                height=data['height'],
-                                weight=data['weight']))
+
+        POKEMONS.append(Pokemon(data['id'],
+                                i['url'],
+                                i['name'],
+                                data['height'],
+                                data['weight']))
 
 
 def main():
-    #task 1
-    make_pokemons(get_content())
-    pok_api = PokeAPI()
-    print(pok_api.get_pokemon(key='ditto'))
+    make_pokemons(get_content(5))
+    pa = PokeAPI()
 
-    #task 2
-    make_pokemons(get_content(50))
-    pok_api2 = PokeAPI()
-    poks = sorted(pok_api2.get_all(get_full=True), key=lambda x: x.get_weight(), reverse=True)
-    print(poks[0])
+    for p in pa.get_all(get_full=False):
+        print(p)
+
+    #task 1
+    # make_pokemons(get_content())
+    # pok_api = PokeAPI()
+    # print(pok_api.get_pokemon(key='ditto'))
+    #
+    # #task 2
+    # make_pokemons(get_content(50))
+    # pok_api2 = PokeAPI()
+    # poks = sorted(pok_api2.get_all(get_full=True), key=lambda x: x.get_weight(), reverse=True)
+    # print(poks[0])
 
 
 main()
--- Assessment/pokemon_models.py
+++ Assessment/pokemon_models.py
@@ -1,29 +1,43 @@
+from dataclasses import dataclass
+import requests
+
 BASE_POKEMONS = []
 POKEMONS = []
 
 
 class BasePokemon:
-    def __init__(self, name: str) -> None:
-        self.__name = name
+    def __init__(self, name: str):
+        self.name = name
 
     def __str__(self) -> str:
-        return f'name: {self.__name}'
+        return f'name: {self.name}'
 
-    def get_name(self) -> str:
-        return self.__name
+    def get_base_name(self) -> str:
+        return self.name
 
 
+class PokemonStats:
+    def __init__(self, *args):
+        self.args = args
+
+    @property
+    def stats(self):
+        my_iter = iter(self.args)
+        return f'hp: {next(my_iter)}; attack: {next(my_iter)}; defense: {next(my_iter)}; ' \
+            f'special_attack: {next(my_iter)}; special_defense: {next(my_iter)}; speed: {next(my_iter)}'
+
+@dataclass
 class Pokemon(BasePokemon):
-    def __init__(self, id:int, name:str, height:int, weight:int) -> None:
-        BasePokemon.__init__(self, name)
+    __id: int
+    __url: str
+    __name: str
+    __height: int
+    __weight: int
 
-        self.__id = id
-        self.__name = name
-        self.__height = height
-        self.__weight = weight
+    stats: PokemonStats = None
 
     def __str__(self) -> str:
-        return f'id: {self.__id}, name: {self.__name}, ' \
+        return f'id: {self.__id}, name: {self.get_name()}, ' \
             f'height: {self.__height}, weight: {self.__weight}'
 
     def get_id(self) -> int:
@@ -35,6 +49,9 @@ class Pokemon(BasePokemon):
     def get_weight(self) -> int:
         return self.__weight
 
+    def get_url(self) -> str:
+        return self.__url
+
 
 class PokeError(Exception):
     pass
@@ -42,18 +59,38 @@ class PokeError(Exception):
 
 class PokeAPI:
     def get_pokemon(self, key: int or str) -> str:
+        pokemon = None
         if isinstance(key, int):
-            return list(filter(lambda x: x.get_id() == key, POKEMONS))[0]
+            pokemon = list(filter(lambda x: x.get_id() == key, POKEMONS))[0]
         elif isinstance(key, str):
-            return list(filter(lambda x: x.get_name() == key, POKEMONS))[0]
+            pokemon = list(filter(lambda x: x.get_name() == key, POKEMONS))[0]
 
-        raise PokeError('такого покемона нет в списке!')
+        else:
+            raise PokeError('такого покемона нет в списке!')
+
+        data = requests.get(pokemon.get_url()).json()['stats']
+
+        pokemon.stats = PokemonStats(data[0]['base_stat'],
+                                     data[1]['base_stat'],
+                                     data[2]['base_stat'],
+                                     data[3]['base_stat'],
+                                     data[4]['base_stat'],
+                                     data[5]['base_stat'])
+        return '\n'.join(["pokemon's DESCRIPTION:",  str(pokemon), "pokemon's STATS:", pokemon.stats.stats, '\n'])
 
     def get_all(self, get_full: bool):
         if not get_full:
             if not BASE_POKEMONS:
                 raise PokeError('список покемонов пуст!')
-            return '\n'.join([str(j) for j in BASE_POKEMONS])
+
+            cur_num = 0
+            pokems_num = len(BASE_POKEMONS)
+
+            while cur_num < pokems_num:
+                yield str(BASE_POKEMONS[cur_num])
+                cur_num += 1
+
+            return None
 
         if not POKEMONS:
             raise PokeError('список покемонов пуст!')
